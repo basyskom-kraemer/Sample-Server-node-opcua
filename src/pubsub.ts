f6900c8fe33d5dd8eaf6d79beeddb9345ef6d2f3
Andreas Kraemer <andreas.kraemer@basyskom.com>
2023-02-16 11:34:28 +0100

    modified:   src/pubsub.ts

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -52,7 +52,7 @@ const createWriterGroup = (name: string, dataSetWriter: MyJsonDataSetWriterDataT
 }
 
 
-const createConnection = (name: string, dataSetName: string, writerGroupName: string, broker: string, id: number): PubSubConnectionDataTypeOptions => {
+const createConnection = (name: string, dataSetName: string, writerGroupName: string, subGroupName: string, broker: string, id: number): PubSubConnectionDataTypeOptions => {
     const dataSetWriter: MyJsonDataSetWriterDataTypeOptions = {
         dataSetFieldContentMask: DataSetFieldContentMask.None,
             // DataSetFieldContentMask.SourceTimestamp |
@@ -79,8 +79,8 @@ const createConnection = (name: string, dataSetName: string, writerGroupName: st
             // requestedDeliveryGuarantee?: BrokerTransportQualityOfService;
             // metaDataQueueName?: UAString;
             // metaDataUpdateTime?: Double;
-            queueName: `${dataPrefix}/${writerGroupName}/identification`,
-            metaDataQueueName: `${metadataPrefix}/${writerGroupName}/identification`,
+            queueName: `${dataPrefix}/${writerGroupName}/${subGroupName}`,
+            metaDataQueueName: `${metadataPrefix}/${writerGroupName}/${subGroupName}`,
             // queueName: `${dataPrefix}/${writerGroupName}/VariableDataSetWriter`,
             // metaDataQueueName: `${metaPrefix}/${writerGroupName}/VariableDataSetWriter`,
             metaDataUpdateTime: 5000
@@ -107,7 +107,7 @@ const createConnection = (name: string, dataSetName: string, writerGroupName: st
 }
 
 
-const createBasicWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
+const createBasicWwIdentificationDataSet = (): PublishedDataSetDataTypeOptions => {
 
     const fields: FieldMetaDataOptions[] = [
         // {
@@ -122,6 +122,8 @@ const createBasicWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
         //     dataSetFieldId?: Guid;
         //     properties?: KeyValuePairOptions[] | null;
         // }
+
+        // Define basic woodworking IDENTIFICATION nodes
         {
             name: "DeviceClass",
             builtInType: DataType.String,
@@ -166,6 +168,8 @@ const createBasicWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
         //     substituteValue?: (VariantLike | null);
         //     metaDataProperties?: (QualifiedNameLike | null)[] | null;
         // }
+
+        // Set basic woodworking IDENTIFICATION nodes
         {
             attributeId: AttributeIds.Value,
             samplingIntervalHint: 1000,
@@ -199,7 +203,314 @@ const createBasicWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
     ]
 
     const publishedDataSet: PublishedDataSetDataTypeOptions = {
-        name: "PublishedBasicWoodWorking",
+        name: "BasicWoodWorkingIdentification",
+        dataSetMetaData: {
+            fields: fields,
+        },
+        dataSetSource: new PublishedDataItemsDataType({
+            publishedData: publishedData,
+        }),
+    };
+    return publishedDataSet;
+}
+
+const createBasicWwFlagsDataSet = (): PublishedDataSetDataTypeOptions => {
+
+    const fields: FieldMetaDataOptions[] = [
+        // {
+        //     name?: UAString;
+        //     description?: (LocalizedTextLike | null);
+        //     fieldFlags?: DataSetFieldFlags;
+        //     builtInType?: Byte;
+        //     dataType?: (NodeIdLike | null);
+        //     valueRank?: Int32;
+        //     arrayDimensions?: UInt32[] | null;
+        //     maxStringLength?: UInt32;
+        //     dataSetFieldId?: Guid;
+        //     properties?: KeyValuePairOptions[] | null;
+        // }
+
+        // Define basic woodworking FLAGS nodes
+        {
+            name: "AirPresent",
+            builtInType: DataType.String,
+            dataType: resolveNodeId("String"),
+        },
+        {
+            name: "Alarm",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Calibrated",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "DustChipSuction",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Emergency",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "EnergySafing",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Error",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "ExternalEmergency",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "FeedRuns",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Hold",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+
+        {
+            name: "LoadingEnabled",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "MachineInit",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "MachineOn",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "MaintananceReqired",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "ManualActivityReqired",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Moving",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "PowerPresent",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "RecipeInHold",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "RecipeInRun",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "RecipeInSetup",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Remote",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Safety",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "WaitLoad",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "WaitUnLoad",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "Warning",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        },
+        {
+            name: "WorkPiecePresent",
+            builtInType: DataType.Boolean,
+            dataType: resolveNodeId("Boolean"),
+        }
+    ]
+
+    const publishedData = [
+        // {
+        //     publishedVariable?: (NodeIdLike | null);
+        //     attributeId?: UInt32;
+        //     samplingIntervalHint?: Double;
+        //     deadbandType?: UInt32;
+        //     deadbandValue?: Double;
+        //     indexRange?: NumericRange;
+        //     substituteValue?: (VariantLike | null);
+        //     metaDataProperties?: (QualifiedNameLike | null)[] | null;
+        // }
+
+        // Set basic woodworking FLAGS nodes
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6048`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6039`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6040`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6049`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6041`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6040`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6042`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6051`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6052`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6053`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6054`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6043`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6044`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6055`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6056`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6057`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6045`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6058`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6046`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6059`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6060`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6061`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6062`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6063`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6047`,
+        },
+        {
+            attributeId: AttributeIds.Value,
+            samplingIntervalHint: 1000,
+            publishedVariable: `ns=34;i=6064`,
+        },
+    ]
+
+    const publishedDataSet: PublishedDataSetDataTypeOptions = {
+        name: "BasicWoodWorkingFlags",
         dataSetMetaData: {
             fields: fields,
         },
@@ -227,6 +538,7 @@ const createFullWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
         //     properties?: KeyValuePairOptions[] | null;
         // }
 
+        // Define full woodworking IDENTIFICATION nodes
         {
             name: "AssetIdNode",
             builtInType: DataType.String,
@@ -300,6 +612,8 @@ const createFullWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
         //     substituteValue?: (VariantLike | null);
         //     metaDataProperties?: (QualifiedNameLike | null)[] | null;
         // }
+
+        // Set full woodworking IDENTIFICATION nodes
         {
             attributeId: AttributeIds.Value,
             samplingIntervalHint: 1000,
@@ -378,11 +692,13 @@ const createFullWoodWorkingDataSet = (): PublishedDataSetDataTypeOptions => {
 export const constructMqttJsonPubSubConfiguration = (broker: string) => {
     const opts: PubSubConfigurationDataTypeOptions = {
         connections: [
-            createConnection("dataSet_BasicWoodWorking", "PublishedBasicWoodWorking", "basic_woodworking", broker, 1),
-            createConnection("dataSet_FullWoodWorking", "PublishedFullWoodWorking", "full_woodworking", broker, 2)
+            createConnection("BasicWoodWorkingIdentification", "BasicWoodWorkingIdentification", "basic_woodworking", "identification", broker, 1),
+            createConnection("BasicWoodWorkingFlags", "BasicWoodWorkingFlags", "basic_woodworking", "state/machine/flags", broker, 2),
+            createConnection("PublishedFullWoodWorking", "PublishedFullWoodWorking", "full_woodworking", "identification", broker, 3)
         ],
         publishedDataSets: [
-            createBasicWoodWorkingDataSet(),
+            createBasicWwIdentificationDataSet(),
+            createBasicWwFlagsDataSet(),
             createFullWoodWorkingDataSet()
         ]
     }
